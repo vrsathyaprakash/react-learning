ceaa41a6042dcffb5a08459625585bfcb72f15da
vrsathyaprakash <sathyaprakash777.sp@gmail.com>
2024-07-31 11:47:06 +0000

    react with vite and basic react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur react |  V R Sathya Prakash</h1>
+  )
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return(
+    <h1>hai aur react with vite | V R Sathya Prakash</h1>
+  )
+}
+
+export default App
